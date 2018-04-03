2b9df45f93e059ad310dbd72d0464f41381952ab
Олег Кузубов <kdmitry2010@yandex.ru>
2018-04-03 23:04:24 +0300

    a/b

--- main.cpp
+++ main.cpp
@@ -1,14 +1,14 @@
  #include <iostream>
 
  using namespace std;
-// Учебная программа
+
  int main()
  {
-
-     int a, b;
-     cout << "Enter A and B: ";
-     cin >> a >> b;
-     cout << "A + B = " << a + b << '\n';
-     cout << a*2;
-     cout << a - b;
+  int a, b;
+  cout << "Enter A and B: ";
+  cin >> a >> b;
+  cout << "A + B = " << a + b << '\n'
+  << "A * 2 = " << a * 2 << '\n'
+  << "A - B = " << a - b << '\n'
+  << "A / B = " << a / b << '\n';
  }
