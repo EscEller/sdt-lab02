4e01245238d415e93dae31f7c10e2c5d2ef4c072
Олег Кузубов <kdmitry2010@yandex.ru>
2018-04-03 22:40:52 +0300

    a*2

--- main.cpp
+++ main.cpp
@@ -4,8 +4,10 @@
 
  int main()
  {
- int a, b;
- cout << "Enter A and B: ";
- cin >> a >> b;
+
+     int a, b;
+     cout << "Enter A and B: ";
+     cin >> a >> b;
      cout << "A + B = " << a + b << '\n';
+     cout << a*2;
  }
